356ab2da6046cac491846feb7d56a8c8dfbc2a85
tiffling <tiffh@bu.edu>
2016-05-04 01:29:41 -0400

    fix crash

--- lib/feature_gate.rb
+++ lib/feature_gate.rb
@@ -1,10 +1,4 @@
-require 'active_support/dependencies'
-
 module FeatureGate
-  # Our host application root path
-  # We set this when the engine is initialized
-  mattr_accessor :app_root
-
   # Yield self on setup for nice config blocks
   def self.setup
     yield self
--- lib/feature_gate/engine.rb
+++ lib/feature_gate/engine.rb
@@ -1,9 +1,4 @@
 module FeatureGate
   class Engine < Rails::Engine
-    initialize 'feature_gate.load_app_instance_data' do |app|
-      FeatureGate.setup do |config|
-        config.app_root = app.root
-      end
-    end
   end
 end
