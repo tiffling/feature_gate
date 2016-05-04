4f467c4f7bd00045ae364e0b1e62ead1a2197353
tiffling <tiffh@bu.edu>
2016-05-04 01:21:04 -0400

    load application root - release 0.1.0

--- lib/feature_gate.rb
+++ lib/feature_gate.rb
@@ -1,4 +1,10 @@
+require 'active_support/dependencies'
+
 module FeatureGate
+  # Our host application root path
+  # We set this when the engine is initialized
+  mattr_accessor :app_root
+
   # Yield self on setup for nice config blocks
   def self.setup
     yield self
--- lib/feature_gate/engine.rb
+++ lib/feature_gate/engine.rb
@@ -1,4 +1,9 @@
 module FeatureGate
   class Engine < Rails::Engine
+    initialize 'feature_gate.load_app_instance_data' do |app|
+      FeatureGate.setup do |config|
+        config.app_root = app.root
+      end
+    end
   end
 end
