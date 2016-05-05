567c0b35c3f42c4ab5738d5f0b2c355ae4d8a62c
tiffling <tiffh@bu.edu>
2016-05-04 23:54:44 -0400

    feature gate convenience page works properly

--- app/controllers/feature_gate/gated_features_controller.rb
+++ app/controllers/feature_gate/gated_features_controller.rb
@@ -1,10 +1,23 @@
 module FeatureGate
   class GatedFeaturesController < ApplicationController
+    layout 'feature_gate/application'
+
     def index
-      @gates = FeatureGate::GatedFeature.all
+      @closed_gates = FeatureGate::GatedFeature.closed
+      @opened_gates = FeatureGate::GatedFeature.opened
     end
 
     def update
+      gate = FeatureGate::GatedFeature.find(params[:id])
+      if params[:gated] == 'true'
+        gate.gate_feature!
+        flash[:notice] = 'Feature has been gated'
+      else
+        gate.deploy_feature!
+        flash[:success] = 'Feature is live!'
+      end
+
+      redirect_to gated_features_path
     end
   end
 end
--- lib/feature_gate/engine.rb
+++ lib/feature_gate/engine.rb
@@ -1,5 +1,9 @@
 module FeatureGate
   class Engine < Rails::Engine
     isolate_namespace FeatureGate
+
+    initializer 'feature_gate.assets.precompile' do |app|
+      app.config.assets.precompile += %w(feature_gate/application.scss.css)
+    end
   end
 end
